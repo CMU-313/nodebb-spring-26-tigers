d43f2dc8b714bddb159a5b7d82e11ac69e48aa21
cnk2024 <chloenahlin@gmail.com>
2026-02-25 12:48:52 -0500

    tests: updated test format, added 'should not anonymize single normal post' test
    Used test and post creation rather than mock json

--- test/anonymous.js
+++ test/anonymous.js
@@ -2,37 +2,63 @@
 
 const assert = require('assert');
 
-const anon = require('../plugins/nodebb-plugin-anon-toggle/library.js');
+const nconf = require('nconf');
+const path = require('path');
+const util = require('util');
+
+const sleep = util.promisify(setTimeout);
+
+const db = require('./mocks/databasemock');
+const topics = require('../src/topics');
+const categories = require('../src/categories');
+const user = require('../src/user');
+const library = require('../plugins/nodebb-plugin-anon-toggle/library')
 
 describe('anonymous', () => {
-    it('should anonymize single post', async () => {
-        const sampleHookData = {
-            post: {
-                pid: 8,
-                uid: 1,
-                tid: 6,
-                content: 'Mock post',
-                timestamp: 1772021528804,
-                anonymous: true,
-                cid: 2
-            },
-            uid: 1,
-            caller: {
-                uid: 1,
-                req: {
-                    uid: 1,
-                    params: {},
-                    method: 'POST',
-                    protocol: 'http',
-                    secure: false,
-                    path: '/category/2/general-discussion',
-                    baseUrl: '',
-                    originalUrl: '/api/v3/topics',
-                }
-            }
-        };
+    let uid
+    let anonPostData1, anonPostData2, postData;
+    let anonTopicData1, anonTopicData2, topicData;
+    let cid;
 
-        const result = await anon.anonymizePostGet(sampleHookData);
+    before(async () => {
+        uid = await user.create({ username: 'Test User' });
+        ({ cid } = await categories.create({
+            name: 'Test Category',
+            description: 'Test category created by testing script',
+        }));
+        ({ topicData: anonTopicData1, postData: anonPostData1 } = await topics.post({
+            uid: uid,
+            cid: cid,
+            title: 'Anon Test Topic Title 1',
+            content: 'The content of test topic',
+            anonymous: 'true'
+        }));
+        ({ topicData: anonTopicData2, postData: anonPostData2 } = await topics.post({
+            uid: uid,
+            cid: cid,
+            title: 'Anon Test Topic Title 2',
+            content: 'The content of test topic',
+            anonymous: 'true'
+        }));
+        ({ topicData, postData } = await topics.post({
+            uid: uid,
+            cid: cid,
+            title: 'Test Topic Title',
+            content: 'The content of test topic'
+        }));
+    });
+
+    it('should anonymize single anonymous post', async () => {
+        const sampleHookData = {post: anonPostData1, uid: uid}
+
+        const result = await library.anonymizePostGet(sampleHookData);
         assert.strictEqual(result.post.uid, 0);
+    });
+    
+    it('should not anonymize single normal post', async () => {
+        const sampleHookData = {post: postData, uid: uid}
+
+        const result = await library.anonymizePostGet(sampleHookData);
+        assert.strictEqual(result.post.uid, uid);
     });
 });
