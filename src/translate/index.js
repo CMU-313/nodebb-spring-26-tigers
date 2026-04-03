6145ec8d9f83925df66d6995a8774df8ff83dc87
cnk2024 <chloenahlin@gmail.com>
2026-04-03 03:27:18 -0400

    fix: handled potential errors

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,10 +4,17 @@
 
 const translatorApi = module.exports;
 
+const TRANSLATOR_API = 'http://17313-team12.s3d.cmu.edu:5000/';
+
 translatorApi.translate = async function (postData) {
-	//  Edit the translator URL below
-	const TRANSLATOR_API = "http://17313-team12.s3d.cmu.edu:5000/"
-	const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-	const data = await response.json();
-	return [data.is_english, data.translated_content];
+	try {
+		const response = await fetch(
+			`${TRANSLATOR_API}/?content=${encodeURIComponent(postData.content)}`
+		);
+		const data = await response.json();
+		return [data.is_english, data.translated_content];
+	} catch (err) {
+		console.error('[translator] Error calling translator service:', err.message, err.constructor.name);
+		return [true, postData.content];
+	}
 };
