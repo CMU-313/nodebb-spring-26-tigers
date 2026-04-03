f9156927bb7af05f4d2ad32f4df56c8dc00c71f9
cnk2024 <chloenahlin@gmail.com>
2026-04-02 21:36:28 -0400

    Revert "Merge pull request #49 from CMU-313/llm-implementation"
    This reverts commit 30a7734637e087816db129b90330af43b837ebb2, reversing
    changes made to 20116d26603e70db1299d149091a63eb112a666e.

--- src/posts/create.js
+++ src/posts/create.js
@@ -9,7 +9,6 @@ const categories = require('../categories');
 const groups = require('../groups');
 const activitypub = require('../activitypub');
 const utils = require('../utils');
-const translate = require('../translate');
 
 module.exports = function (Posts) {
 	Posts.create = async function (data) {
@@ -19,8 +18,6 @@ module.exports = function (Posts) {
 		const timestamp = data.timestamp || Date.now();
 		const isMain = data.isMain || false;
 		let hasAttachment = false;
-		const [isEnglish, translatedContent] = await translate.translate(data);
-
 
 		if (!uid && parseInt(uid, 10) !== 0) {
 			throw new Error('[[error:invalid-uid]]');
@@ -31,7 +28,7 @@ module.exports = function (Posts) {
 		}
 
 		const pid = data.pid || await db.incrObjectField('global', 'nextPid');
-		let postData = { pid, uid, tid, content, sourceContent, timestamp, isEnglish, translatedContent};
+		let postData = { pid, uid, tid, content, sourceContent, timestamp };
 
 		if (data.anonymous != undefined || data.anonymous != null) {
 			postData.anonymous = data.anonymous;
